0fcaa21b93d4b9562be0e4a4a7827a68bbb880f8
sumathiD <sumathi@sdtech.io>
2022-04-14 16:47:38 +0530

    login page updated

--- src/pages/LoginDemo.tsx
+++ src/pages/LoginDemo.tsx
@@ -1,55 +1,135 @@
-import React from 'react';
-import { useNavigate,Link} from 'react-router-dom';
-import {Avatar, Grid,Paper, TextField} from '@material-ui/core'
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { Avatar, Grid, Paper, TextField } from '@material-ui/core'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import * as yup from 'yup';
+import { useFormik } from 'formik';
+import axios from 'axios';
 
+
+const validationSchema = yup.object({
+  Email: yup
+    .string()
+    // .email("Enter valid email")
+    .required("email required"),
+  Password: yup
+  .string()
+  .required(" password required ")
+  
+})
 
 function LoginDemo() {
-  let navigate= useNavigate();
-  const paperStyle={padding:20,height:'60vh',width:280,margin:'20px auto'};
-  const buttonStyle={margin:'8px 0'}
-  const avatarStyle={backgroundColor:"#305281"}
+
+  let navigate = useNavigate();
+  const paperStyle = { padding: 20, height: '60vh', width: 280, margin: '20px auto' };
+  const buttonStyle = { margin: '8px 0' }
+  const avatarStyle = { backgroundColor: "#305281" }
+
+  const formik = useFormik({
+    initialValues: {
+      Email: '',
+      Password: "",
+      confirmPassword: ""
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log(values.Email, values.Password);
+
+      axios.post(`http://localhost:3000/auth/login`, {
+        "username": values.Email,
+        "password": values.Password
+      })
+        .then((res: any) => {
+          // localStorage.setItem{'login', JSON.stringify(
+          //   {
+          //     login: true,
+          //     token: res.data.access_token
+          //   }
+          // )
+
+          
+          console.log(res);
+          console.log(res.data.access_token);
+        })
+    }
+  })
+  
   return (
     <Grid>
-       {/* <input type="text"></input><br/><br/>
+      {/* <input type="text"></input><br/><br/>
        <button onClick={() => navigate("../dashboard") }>submit</button> */}
-<Paper elevation={10} style={paperStyle}>
-  <Grid className='center'>
-  <Avatar className='align' style={avatarStyle} ><LockOutlinedIcon/></Avatar>
-      <h2>Log in</h2>
-  </Grid>
-  <TextField label="Email" placeholder='Email' fullWidth required/>
-  <TextField label="Password" placeholder='Password' type="password" fullWidth required/>
-  <FormControlLabel
-        control={
-          <Checkbox
-            name="checkedB"
-            color="primary"
+      <Paper elevation={10} style={paperStyle}>
+        <Grid className='center'>
+          <Avatar className='align' style={avatarStyle} ><LockOutlinedIcon /></Avatar>
+          <h2>Log in</h2>
+        </Grid>
+        <form onSubmit={formik.handleSubmit}>
+
+          <TextField
+            fullWidth
+            label="Email"
+            placeholder='Email'
+            id="Email"
+            name="Email"
+            helperText={formik.touched.Email && formik.errors.Email}
+            value={formik.values.Email}
+            error={formik.touched.Email && Boolean(formik.errors.Email)}
+            onChange={formik.handleChange} />
+          <TextField
+            label="Password"
+            placeholder='Password'
+            type="password"
+            onChange={formik.handleChange}
+            name="Password"
+            error={formik.touched.Password && Boolean(formik.errors.Password)}
+            helperText={formik.touched.Password && formik.errors.Password}
+            value={formik.values.Password}
+            fullWidth />
+
+          <FormControlLabel
+            control={
+              <Checkbox
+                name="checkedB"
+                color="primary"
+              />
+            }
+            label="Remember me"
           />
-        }
-        label="Remember me"
-      />
-      <Button variant="contained" color="primary" fullWidth style={buttonStyle} onClick={() => navigate("/dashboard") }>
-       LOGIN
-      </Button>
-      {/* <Typography>
+          <Button
+            variant="contained"
+            color="secondary"
+            fullWidth
+            style={buttonStyle}
+            type='submit'>
+            Login
+          </Button>
+          {/* <Button 
+          variant="contained" 
+          color="primary" 
+          fullWidth 
+          style={buttonStyle} 
+          onClick={() => navigate("/dashboard")}>
+          LOGIN
+        </Button> */}
+        </form>
+        {/* <Typography>
   <Link href="#">
     forgot password ?
   </Link>
 </Typography> */}
-<Typography>
-  Do you have an account ?
-  <Link to ="/signup">
-   Sign up 
-  </Link>
-</Typography>
-</Paper>
-      </Grid>
-    
+        <Typography>
+          Do you have an account ?
+          <Link to="/signup">
+            Sign up
+          </Link>
+        </Typography>
+      </Paper>
+    </Grid>
+
   )
 }
 
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { useNavigate,} from 'react-router-dom';
-import {Avatar, Grid,Paper, TextField,Link} from '@material-ui/core'
+import { useNavigate, } from 'react-router-dom';
+import { Avatar, Grid, Paper, TextField, Link } from '@material-ui/core'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
@@ -8,19 +8,20 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import * as yup from 'yup';
 // import {useForm} from 'react-hook-form';
-import {Formik,Field,Form,ErrorMessage, useFormik} from 'formik';
+import { Formik, Field, Form, ErrorMessage, useFormik } from 'formik';
+import axios from 'axios';
 
-  const paperStyle={padding:20,height:'65vh',width:280,margin:'20px auto'};
-  const buttonStyle={margin:'8px 0'}
-  const avatarStyle={backgroundColor:"#305281"}
-  const validationSchema = yup.object({
-    Email: yup
+const paperStyle = { padding: 20, height: '65vh', width: 280, margin: '20px auto' };
+const buttonStyle = { margin: '8px 0' }
+const avatarStyle = { backgroundColor: "#305281" }
+const validationSchema = yup.object({
+  Email: yup
     .string()
-    .email("Enter valid email")
+    // .email("Enter valid email")
     .required("email required"),
-    Password:yup.string().min(8,"Enter valid email").required(" password required "),
-    confirmPassword:yup.string().oneOf([yup.ref('Password')],"password not matched").required("required")
-  })
+  Password: yup.string().min(3, "Enter valid email").required(" password required "),
+  confirmPassword: yup.string().oneOf([yup.ref('Password')], "password not matched").required("required")
+})
 function Signup() {
   // const [values,setValues]=useState({
   //   Email:'',
@@ -33,16 +34,29 @@ function Signup() {
   //   [e.target.name]:e.target.value, })
   // // console.log(e.target.name,e.target.value);
   // }
-  let navigate= useNavigate();
+  let navigate = useNavigate();
   const formik = useFormik({
-    initialValues:{
-      Email:'',
-      Password:"",
-      confirmPassword:""
+    initialValues: {
+      Email: '',
+      Password: "",
+      confirmPassword: ""
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
+      // axios.post('/user/register',{
+      //   "username": values.Email,
+      //   "password": values.Password
+      // }).then()
       // alert(JSON.stringify(values, null, 2));
+      axios.post(`http://localhost:3000/user/register`, {
+        "username": values.Email,
+        "password": values.Password
+      })
+        .then(res => {
+          console.log(res);
+          console.log(res.data);
+          navigate("/login")
+        })
     },
   })
   // const onSubmit=(values:any,props:any)=>(
@@ -50,44 +64,33 @@ function Signup() {
   // )
   return (
     <Grid>
-       {/* <input type="text"></input><br/><br/>
+      {/* <input type="text"></input><br/><br/>
        <button onClick={() => navigate("../dashboard") }>submit</button> */}
-<Paper elevation={10} style={paperStyle}>
-  <Grid className='center'>
-  <Avatar className='align' style={avatarStyle} ><LockOutlinedIcon/></Avatar>
-      <h2>Sign up</h2>
-  </Grid>
-<form  onSubmit={formik.handleSubmit}>
-<TextField
-fullWidth
-label="Email"
-placeholder='Email'
-id="Email"
-name="Email"
-helperText={ formik.touched.Email && formik.errors.Email}
-value={formik.values.Email}
-error={formik.touched.Email && Boolean(formik.errors.Email)}
-onChange={formik.handleChange}  />
-  <TextField label="Password" placeholder='Password' type="password" onChange={formik.handleChange} name="Password"  error={formik.touched.Password && Boolean(formik.errors.Password)} helperText={ formik.touched.Password && formik.errors.Password} value={formik.values.Password} fullWidth/>
-  <TextField label="confirm Password" placeholder='confirm Password' type="password" onChange={formik.handleChange} name="confirmPassword"  helperText={ formik.touched.confirmPassword && formik.errors.confirmPassword} value={formik.values.confirmPassword} error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)} fullWidth/>
-      {/* <Button variant="contained" color="secondary" fullWidth style={buttonStyle} onClick={() => navigate('/') }>
-       SIGN UP </Button> */}
-      <Button variant="contained" color="secondary" fullWidth style={buttonStyle} type='submit'>
-       SIGN UP </Button>
-      {/* <Typography>
-  <Link href="#">z
-    forgot password ?
-  </Link>
-</Typography> */}
-{/* <Typography>
-  Do you have an account ?
-  <Link href="#">
-   Sign up
-  </Link>
-</Typography> */}
-</form>
-</Paper>
-      </Grid>
+      <Paper elevation={10} style={paperStyle}>
+        <Grid className='center'>
+          <Avatar className='align' style={avatarStyle} ><LockOutlinedIcon /></Avatar>
+          <h2>Sign up</h2>
+        </Grid>
+        <form onSubmit={formik.handleSubmit}>
+          <TextField
+            fullWidth
+            label="Email"
+            placeholder='Email'
+            id="Email"
+            name="Email"
+            helperText={formik.touched.Email && formik.errors.Email}
+            value={formik.values.Email}
+            error={formik.touched.Email && Boolean(formik.errors.Email)}
+            onChange={formik.handleChange} />
+          <TextField label="Password" placeholder='Password' type="password" onChange={formik.handleChange} name="Password" error={formik.touched.Password && Boolean(formik.errors.Password)} helperText={formik.touched.Password && formik.errors.Password} value={formik.values.Password} fullWidth />
+          <TextField label="confirm Password" placeholder='confirm Password' type="password" onChange={formik.handleChange} name="confirmPassword" helperText={formik.touched.confirmPassword && formik.errors.confirmPassword} value={formik.values.confirmPassword} error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)} fullWidth />
+
+          <Button variant="contained" color="secondary" fullWidth style={buttonStyle} type='submit'>
+            SIGN UP </Button>
+
+        </form>
+      </Paper>
+    </Grid>
   )
 }
 export default Signup
