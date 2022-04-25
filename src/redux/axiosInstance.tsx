a0a3f7ac971ba1b86308d8b1885a237b34650720
sumathiD <sumathi@sdtech.io>
2022-04-25 15:05:32 +0530

    Update axiosInstance.tsx

--- src/redux/axiosInstance.tsx
+++ src/redux/axiosInstance.tsx
@@ -8,8 +8,5 @@ const instance = axios.create({
         'Authorization': `Bearer ${localStorage.getItem('user')}`
     }
 });
-    
-    // axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('user')}`;
-    // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
 export default instance;
-//instance.defaults.headers.common
